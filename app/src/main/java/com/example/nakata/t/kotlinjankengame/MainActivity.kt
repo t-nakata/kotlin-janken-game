2e1b225fe22bd115a6bbe89d353de3dfb4b6d144
t_nakata <git.nakata0615@gmial.com>
2018-10-13 14:24:51 +0900

    lesson01: hello world!

---
+++ app/src/main/java/com/example/nakata/t/kotlinjankengame/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nakata.t.kotlinjankengame
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
